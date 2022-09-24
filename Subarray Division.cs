eb89712480e76b514ad77182b67ac3ef1d64b63d
BerserkBlaze <andruth17@hotmail.com>
2022-09-24 16:02:59 -0500

    Subarray Division - Solution C#

---
+++ Subarray Division.cs
@@ -0,0 +1,79 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'birthday' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. INTEGER_ARRAY s
+     *  2. INTEGER d
+     *  3. INTEGER m
+     */
+
+    public static int birthday(List<int> s, int d, int m)
+    {
+        int numberResult=0,sumAux=0;
+        if(m==1){
+            if(s[0]==d){
+                numberResult++;
+            }
+        }    
+        else{
+            
+            for(int i=0;i<s.Count;i++){
+                sumAux=0;
+                if(i+m<=s.Count){
+                    for(int j=i;j<i+m;j++){
+                        sumAux+=s[j];
+                    }   
+                    if(sumAux==d){
+                        numberResult++;
+                    }
+                }
+            }
+        }
+        
+        return numberResult;
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+        string[] firstMultipleInput = Console.ReadLine().TrimEnd().Split(' ');
+
+        int d = Convert.ToInt32(firstMultipleInput[0]);
+
+        int m = Convert.ToInt32(firstMultipleInput[1]);
+
+        int result = Result.birthday(s, d, m);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
